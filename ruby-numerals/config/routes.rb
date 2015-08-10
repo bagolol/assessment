faaa475b5e339267f84f1fecf37d306bf0053b05
Rocco <roccorighi@hotmail.com>
2015-08-10 15:51:54 +0100

    updates routes with numbers as index

--- ruby-numerals/config/routes.rb
+++ ruby-numerals/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'numbers' => 'numbers#index'
+
+  resources :numbers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
