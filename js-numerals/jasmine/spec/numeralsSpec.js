d62b62c00f106a3fdb0f88462e4d49182cd47994
Rocco <roccorighi@hotmail.com>
2015-08-16 14:29:30 +0100

    adds logic and tests for spelling numbers up to 19

--- js-numerals/jasmine/spec/numeralsSpec.js
+++ js-numerals/jasmine/spec/numeralsSpec.js
@@ -7,7 +7,17 @@ describe("Speller", function() {
     speller = new Speller();
   });
 
-  it("returns 'one' when 1 is submitted", function() {
-    expect(speller.spellNumber(1)).toEqual("one");
+  describe("knows how to spell numbers from 1 to 19", function(){
+    it("returns 'one' when 1 is submitted", function() {
+      expect(speller.spellNumber(1)).toEqual("one");
+    });
+
+    it("returns 'nine' when 9 is submitted", function() {
+      expect(speller.spellNumber(9)).toEqual("nine");
+    });
+
+    it("returns 'nineteen' when 19 is submitted", function() {
+      expect(speller.spellNumber(19)).toEqual("nineteen");
+    });
   });
 });
--- js-numerals/jasmine/src/Speller.js
+++ js-numerals/jasmine/src/Speller.js
@@ -1,7 +1,28 @@
 function Speller() {
+  this.writtenNumbers = {
+                          1: "one",
+                          2: "two",
+                          3: "three",
+                          4: "four",
+                          5: "five",
+                          6: "six",
+                          7: "seven",
+                          8: "eight",
+                          9: "nine",
+                          10: "ten",
+                          11: "eleven",
+                          12: "twelve",
+                          13: "thirteen",
+                          14: "fourteen",
+                          15: "fifteen",
+                          16: "sixteen",
+                          17: "seventeen",
+                          18: "eighteen",
+                          19: "nineteen",
+                        }
 }
 Speller.prototype.spellNumber = function(integer) {
-  return "one"
+  return this.writtenNumbers[integer]
 };
 
 // module.exports = Speller;
