7ded138b41e814f3093eb297afe3cb30e607bc51
Rocco <roccorighi@hotmail.com>
2015-08-17 18:28:25 +0100

    updates formatting

--- js-numerals/jasmine/spec/numeralsSpec.js
+++ js-numerals/jasmine/spec/numeralsSpec.js
@@ -1,62 +1,61 @@
 describe("Speller", function() {
-  // var Speller = require('../src/Speller');
-  var speller;
+    var speller;
 
 
-  beforeEach(function() {
-    speller = new Speller();
-  });
-
-  describe("knows how to spell numbers from 1 to 19", function(){
-    it("returns 'one' when 1 is submitted", function() {
-      expect(speller.spellNumber(1)).toEqual("one");
+    beforeEach(function() {
+        speller = new Speller();
     });
 
-    it("returns 'nine' when 9 is submitted", function() {
-      expect(speller.spellNumber(9)).toEqual("nine");
+    describe("knows how to spell numbers from 1 to 19", function() {
+        it("returns 'one' when 1 is submitted", function() {
+            expect(speller.spellNumber(1)).toEqual("one");
+        });
+
+        it("returns 'nine' when 9 is submitted", function() {
+            expect(speller.spellNumber(9)).toEqual("nine");
+        });
+
+        it("returns 'nineteen' when 19 is submitted", function() {
+            expect(speller.spellNumber(19)).toEqual("nineteen");
+        });
     });
 
-    it("returns 'nineteen' when 19 is submitted", function() {
-      expect(speller.spellNumber(19)).toEqual("nineteen");
+    describe("knows how to spell tens", function() {
+        it("returns 'twenty' when 20 is submitted", function() {
+            expect(speller.spellNumber(20)).toEqual("twenty");
+        });
+        it("returns 'fifty' when 50 is submitted", function() {
+            expect(speller.spellNumber(50)).toEqual("fifty");
+        });
     });
-  });
-
-  describe("knows how to spell tens", function(){
-     it("returns 'twenty' when 20 is submitted", function() {
-      expect(speller.spellNumber(20)).toEqual("twenty");
+    describe("knows how to spell numbers up until 100", function() {
+        it("returns 'seventy-seven' when 77 is submitted", function() {
+            expect(speller.spellNumber(77)).toEqual("seventy-seven");
+        });
+        it("returns 'fity-two' when 52 is submitted", function() {
+            expect(speller.spellNumber(52)).toEqual("fifty-two");
+        });
     });
-    it("returns 'fifty' when 50 is submitted", function() {
-      expect(speller.spellNumber(50)).toEqual("fifty");
+    describe("knows how to spell numbers up until 999", function() {
+        it("returns 'one hundred seventy-seven' when 177 is submitted", function() {
+            expect(speller.spellNumber(177)).toEqual("one hundred seventy-seven");
+        });
+        it("returns 'four hundred two' when 402 is submitted", function() {
+            expect(speller.spellNumber(402)).toEqual("four hundred two");
+        });
+        it("returns 'one hundred' when 100 is submitted", function() {
+            expect(speller.spellNumber(100)).toEqual("one hundred");
+        });
     });
-  });
-  describe("knows how to spell numbers up until 100", function(){
-     it("returns 'seventy-seven' when 77 is submitted", function() {
-      expect(speller.spellNumber(77)).toEqual("seventy-seven");
+    describe("knows how to spell numbers up until 9999", function() {
+        it("returns 'seventeen hundred seventy-seven' when 1777 is submitted", function() {
+            expect(speller.spellNumber(1777)).toEqual("seventeen hundred seventy-seven");
+        });
+        it("returns 'four thousand four hundred two' when 4402 is submitted", function() {
+            expect(speller.spellNumber(4402)).toEqual("four thousand four hundred two");
+        });
+        it("returns 'nine thousand' when 9000 is submitted", function() {
+            expect(speller.spellNumber(9000)).toEqual("nine thousand");
+        });
     });
-    it("returns 'fity-two' when 52 is submitted", function() {
-      expect(speller.spellNumber(52)).toEqual("fifty-two");
-    });
-  });
-  describe("knows how to spell numbers up until 999", function(){
-     it("returns 'one hundred seventy-seven' when 177 is submitted", function() {
-      expect(speller.spellNumber(177)).toEqual("one hundred seventy-seven");
-    });
-    it("returns 'four hundred two' when 402 is submitted", function() {
-      expect(speller.spellNumber(402)).toEqual("four hundred two");
-    });
-    it("returns 'one hundred' when 100 is submitted", function() {
-      expect(speller.spellNumber(100)).toEqual("one hundred");
-    });
-  });
-  describe("knows how to spell numbers up until 9999", function(){
-     it("returns 'seventeen hundred seventy-seven' when 1777 is submitted", function() {
-      expect(speller.spellNumber(1777)).toEqual("seventeen hundred seventy-seven");
-    });
-    it("returns 'four thousand four hundred two' when 4402 is submitted", function() {
-      expect(speller.spellNumber(4402)).toEqual("four thousand four hundred two");
-    });
-    it("returns 'nine thousand' when 9000 is submitted", function() {
-      expect(speller.spellNumber(9000)).toEqual("nine thousand");
-    });
-  });
 });
--- js-numerals/jasmine/src/Speller.js
+++ js-numerals/jasmine/src/Speller.js
@@ -1,33 +1,33 @@
 function Speller() {
-  this.writtenNumbers = {
-                          1: "one",
-                          2: "two",
-                          3: "three",
-                          4: "four",
-                          5: "five",
-                          6: "six",
-                          7: "seven",
-                          8: "eight",
-                          9: "nine",
-                          10: "ten",
-                          11: "eleven",
-                          12: "twelve",
-                          13: "thirteen",
-                          14: "fourteen",
-                          15: "fifteen",
-                          16: "sixteen",
-                          17: "seventeen",
-                          18: "eighteen",
-                          19: "nineteen",
-                          20: "twenty",
-                          30: "thirty",
-                          40: "forty",
-                          50: "fifty",
-                          60: "sixty",
-                          70: "seventy",
-                          80: "eighty",
-                          90: "ninety"
-                        }
+    this.writtenNumbers = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+    }
 };
 
 
@@ -35,39 +35,36 @@ var units, tens, textLeft, dividers, firstDigit;
 
 Speller.prototype.spellNumber = function(integer) {
 
+    if (this.writtenNumbers[integer]) {
+        return this.writtenNumbers[integer]
+    } else if (integer / 1000 >= 2) {
 
-  if(this.writtenNumbers[integer]) {
-    return this.writtenNumbers[integer]
-  } else if (integer/1000 >= 2) {
+        return this.writeSentence(integer, 1000);
 
-    return this.writeSentence(integer, 1000);
+    } else if (integer / 100 >= 1) {
 
-  } else if (integer/100 >= 1) {
+        return this.writeSentence(integer, 100);
 
-    return this.writeSentence(integer, 100);
-
-  } else {
-    units = integer%10;
-    tens = integer - units;
-    textStart = this.spellNumber(tens);
-    return textStart + '-' + this.writtenNumbers[units];
-  };
+    } else {
+        units = integer % 10;
+        tens = integer - units;
+        textStart = this.spellNumber(tens);
+        return textStart + '-' + this.writtenNumbers[units];
+    };
 };
 
-Speller.prototype.writeSentence = function(integer, divider){
+Speller.prototype.writeSentence = function(integer, divider) {
 
-  dividers = {100: " hundred", 1000: " thousand"};
-  units = integer%divider;
-  firstDigit = (integer-units)/divider;
-  var textStart = this.writtenNumbers[firstDigit];
-  var nameNum = dividers[divider];
-  if(units === 0){
-    return textStart + nameNum;
-  } else {
-    textLeft = this.spellNumber(units);
-    return textStart + nameNum + ' ' + textLeft;
-  };
+    this.writtenNumbers[100] = " hundred"
+    this.writtenNumbers[1000] = " thousand"
+    units = integer % divider;
+    firstDigit = (integer - units) / divider;
+    var textStart = this.writtenNumbers[firstDigit];
+    var nameNum = this.writtenNumbers[divider];
+    if (units === 0) {
+        return textStart + nameNum;
+    } else {
+        textLeft = this.spellNumber(units);
+        return textStart + nameNum + ' ' + textLeft;
+    };
 };
-
-
-
