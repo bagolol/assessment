b12c4c228fe13a5050097ae7345a121830374181
Rocco <roccorighi@hotmail.com>
2015-08-17 12:49:30 +0100

    updates test spec to re-activate previously skipped tests

--- js-numerals/jasmine/spec/numeralsSpec.js
+++ js-numerals/jasmine/spec/numeralsSpec.js
@@ -8,40 +8,40 @@ describe("Speller", function() {
   });
 
   describe("knows how to spell numbers from 1 to 19", function(){
-    xit("returns 'one' when 1 is submitted", function() {
+    it("returns 'one' when 1 is submitted", function() {
       expect(speller.spellNumber(1)).toEqual("one");
     });
 
-    xit("returns 'nine' when 9 is submitted", function() {
+    it("returns 'nine' when 9 is submitted", function() {
       expect(speller.spellNumber(9)).toEqual("nine");
     });
 
-    xit("returns 'nineteen' when 19 is submitted", function() {
+    it("returns 'nineteen' when 19 is submitted", function() {
       expect(speller.spellNumber(19)).toEqual("nineteen");
     });
   });
 
   describe("knows how to spell tens", function(){
-     xit("returns 'twenty' when 20 is submitted", function() {
+     it("returns 'twenty' when 20 is submitted", function() {
       expect(speller.spellNumber(20)).toEqual("twenty");
     });
-    xit("returns 'fifty' when 50 is submitted", function() {
+    it("returns 'fifty' when 50 is submitted", function() {
       expect(speller.spellNumber(50)).toEqual("fifty");
     });
   });
   describe("knows how to spell numbers up until 100", function(){
-     xit("returns 'seventy-seven' when 77 is submitted", function() {
+     it("returns 'seventy-seven' when 77 is submitted", function() {
       expect(speller.spellNumber(77)).toEqual("seventy-seven");
     });
-    xit("returns 'fity-two' when 52 is submitted", function() {
+    it("returns 'fity-two' when 52 is submitted", function() {
       expect(speller.spellNumber(52)).toEqual("fifty-two");
     });
   });
   describe("knows how to spell numbers up until 999", function(){
-     xit("returns 'one hundred seventy-seven' when 177 is submitted", function() {
+     it("returns 'one hundred seventy-seven' when 177 is submitted", function() {
       expect(speller.spellNumber(177)).toEqual("one hundred seventy-seven");
     });
-    xit("returns 'four hundred two' when 402 is submitted", function() {
+    it("returns 'four hundred two' when 402 is submitted", function() {
       expect(speller.spellNumber(402)).toEqual("four hundred two");
     });
     it("returns 'one hundred' when 100 is submitted", function() {
@@ -49,13 +49,13 @@ describe("Speller", function() {
     });
   });
   describe("knows how to spell numbers up until 9999", function(){
-     xit("returns 'seventeen hundred seventy-seven' when 1777 is submitted", function() {
+     it("returns 'seventeen hundred seventy-seven' when 1777 is submitted", function() {
       expect(speller.spellNumber(1777)).toEqual("seventeen hundred seventy-seven");
     });
-    xit("returns 'four thousand four hundred two' when 4402 is submitted", function() {
+    it("returns 'four thousand four hundred two' when 4402 is submitted", function() {
       expect(speller.spellNumber(4402)).toEqual("four thousand four hundred two");
     });
-    xit("returns 'nine thousand' when 9000 is submitted", function() {
+    it("returns 'nine thousand' when 9000 is submitted", function() {
       expect(speller.spellNumber(9000)).toEqual("nine thousand");
     });
   });
